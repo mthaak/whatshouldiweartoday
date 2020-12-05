ffc4ff17598e6c992b7b97bba4e166efa5668f79
mthaak <martin.ter.haak@gmail.com>
2020-12-05 16:19:51 +0100

    Make LocationService use AsyncStorage

--- services/LocationService.ts
+++ services/LocationService.ts
@@ -1,6 +1,6 @@
 import * as ExpoLocation from 'expo-location';
 
-import { store } from './store';
+import * as Store from './store';
 
 import Location from './Location'
 
@@ -14,7 +14,7 @@ let permission = null;
 export async function requestPermission() {
   permission = (await ExpoLocation.requestPermissionsAsync()).status;
   if (permission !== 'granted')
-    console.log('Permission to use location not given by user')
+    console.error('Permission to use location not given by user')
 }
 
 export async function updateLocation() {
@@ -24,12 +24,14 @@ export async function updateLocation() {
     let addressResults = await ExpoLocation.reverseGeocodeAsync(location.coords);
 
     let city = getCity(addressResults)
+    let profile = Store.retrieveProfile()
     if (city) {
-      store.profile.home = new Location(location.coords.latitude, location.coords.longitude, city);
+      profile.home = new Location(location.coords.latitude, location.coords.longitude, city);
     } else {
-      store.profile.home = new Location(location.coords.latitude, location.coords.longitude, city);
-      console.log('Could not extract city from reverse geocode')
+      profile.home = 'Unknown';
+      console.error('Could not extract city from reverse geocode')
     }
+    Store.saveProfile(profile);
   }
 }
 
