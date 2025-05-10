f8f3da896020adf0d13b6934cfe17bd19f0bcad9
Martin ter Haak <6129322+mthaak@users.noreply.github.com>
2025-05-10 15:56:39 +0100

    Fix ESLint config

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -14,7 +14,8 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ["tsconfig.json"],
+    project: ["./tsconfig.eslint.json"],
+    tsconfigRootDir: __dirname,
     sourceType: "module",
   },
   ignorePatterns: [
